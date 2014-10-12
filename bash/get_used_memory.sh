a9b4226160516165401cb2836c0f7caf1ae55ef8
Danny McClanahan <danieldmcclanahan@gmail.com>
2014-10-12 01:37:53 -0500

    test_commit

--- bash/get_used_memory.sh
+++ bash/get_used_memory.sh
@@ -1,13 +1,14 @@
-#!/bin/zsh
+#!/bin/bash
 
-# due to annoying syntax issues /bin/zsh is required past the crunchbang
+# tr is annoying here, not sure why it's necessary
 
-used_and_free_mem=$(echo "$(free -m | grep buffers/cache | grep -o "[[:digit:]]*")")
-used_mem=$(echo $used_and_free_mem | head -n1)
-free_mem=$(echo $used_and_free_mem | tail -n1)
-total_mem=$(echo "$used_mem + $free_mem" | bc)
+used_and_free_mem="$(free -m | grep "buffers/cache" | grep -o "[[:digit:]]*")"
+used_mem="$(echo $used_and_free_mem | tr [[:space:]]* "\n" | head -n1)"
+free_mem="$(echo $used_and_free_mem | tr [[:space:]]* "\n" | tail -n1)"
+total_mem="$(echo "$used_mem + $free_mem" | bc)"
+ratio="$(echo "scale=3; $used_mem / $total_mem * 100" | bc)"
 
 echo -e "Used:\t$used_mem MB"
 echo -e "Free:\t$free_mem MB"
 echo -e "Total:\t$total_mem MB"
-echo "$(echo "scale=3;$used_mem/$total_mem*100" | bc)% used."
+echo "$ratio% used."
--- bash/size_in_bytes.sh
+++ bash/size_in_bytes.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
 du -hsb $@ | cut -f1 | grep -o "[0-9,\.]*"
