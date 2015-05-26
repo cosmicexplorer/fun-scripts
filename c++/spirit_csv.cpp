20d10254fc7e3a6fa577d29de035ddc563dcb5cb
Danny McClanahan <danieldmcclanahan@gmail.com>
2015-05-26 03:51:25 -0700

    got spirit csv parsing to work!

--- c++/spirit_csv.cpp
+++ c++/spirit_csv.cpp
@@ -1,11 +1,17 @@
 // following example from:
-// http://www.boost.org/doc/libs/1_58_0/libs/spirit/example/qi/employee.cpp
+// http://www.boost.org/doc/libs/1_58_0/libs/spirit/example/qi/employee.cpp, and
+// num_list4.cpp, and others
+
+#define BOOST_SPIRIT_DEBUG 1
+#define BOOST_SPIRIT_DEBUG_PRINT_SOME 200
+#define BOOST_SPIRIT_DEBUG_OUT std::cerr
 
 // std includes
 #include <iostream>
 #include <string>
 // boost includes
 #include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/phoenix_stl.hpp>
 
 namespace frontend {
 namespace spirit = boost::spirit;
@@ -31,26 +37,37 @@ struct cursor {
 }
 
 // adapt struct to boost fusion
-BOOST_FUSION_ADAPT_STRUCT(frontend::cursor, (std::string, file),
-                          (unsigned long long, offset),
-                          (unsigned long long, line), (unsigned long long, col),
-                          (std::string, reference_type),
-                          (std::string, specifier), (std::string, type),
-                          (std::string, language), (std::string, name),
-                          (std::string, scope));
+BOOST_FUSION_ADAPT_STRUCT(
+    frontend::cursor,
+    (std::string, file)(unsigned long long, offset)(unsigned long long, line)(
+        unsigned long long, col)(std::string, reference_type)(std::string,
+                                                              specifier)(
+        std::string, type)(std::string, language)(std::string,
+                                                  name)(std::string, scope));
 
+// note: blank_type is so that newlines aren't counted as skippable, because
+// they are significant for csv! however, typically you'll be wanting to use
+// boost::spirit::ascii::space as your whitespace operator if you really do not
+// care about whitespace
 namespace frontend {
 template <typename Iterator>
-struct cursor_parser : qi::grammar<Iterator, cursor(), qi::blank_type> {
+struct cursor_parser
+    : public qi::grammar<Iterator, std::vector<cursor>(), qi::blank_type> {
   qi::rule<Iterator, std::string(), qi::blank_type> quoted_string;
   qi::rule<Iterator, cursor(), qi::blank_type> start;
+  qi::rule<Iterator, std::vector<cursor>(), qi::blank_type> vec;
 
-  cursor_parser() : cursor_parser::base_type(start) {
+  cursor_parser() : cursor_parser::base_type(vec) {
     using qi::uint_;
+    using qi::eol;
     using qi::lexeme;
+    using qi::_1;
     using ascii::char_;
+    using boost::phoenix::push_back;
+    using boost::phoenix::ref;
+    using boost::spirit::_val;
 
-    quoted_string %= lexeme['"' >> +(char_ - '"') >> '"'];
+    quoted_string %= lexeme['"' >> *(char_ - '"') >> '"'];
 
     start %=
         // file
@@ -73,32 +90,47 @@ struct cursor_parser : qi::grammar<Iterator, cursor(), qi::blank_type> {
         quoted_string >> ',' >>
         // scope
         quoted_string;
+
+    vec %= start % eol;
+
+    quoted_string.name("qs");
+    start.name("s");
+    vec.name("v");
+    BOOST_SPIRIT_DEBUG_NODES((quoted_string)(start)(vec))
   }
 };
-
-// template <typename Iterator>
-// bool parse_cursors_as_vector(Iterator first, Iterator last,
-//                              std::vector<cursor> &v) {
-//   bool res = qi::phrase_parse(first, last, (cursor_parser % qi::eol), )
-// }
 }
 
+// try cat file | ./spirit_csv, or alternatively,
+/*
+echo \
+  "\"f\",111,222,333,\"ref_type\",\"spc\",\"type\",\"lan\",\"name\",\"scop\"" \
+  | ./spirit_csv
+*/
 int main() {
-  std::string s;
-  frontend::cursor c;
-  while (getline(std::cin, s)) {
-    s += '\n';
-    c = {}; // zero it out
-    if (phrase_parse(s.cbegin(), s.cend(),
-                     frontend::cursor_parser<std::string::const_iterator>(),
-                     boost::spirit::qi::blank, c)) {
+  boost::spirit::istream_iterator start(std::cin >> std::noskipws), end;
+  std::vector<frontend::cursor> v;
+  if (phrase_parse(start, end,
+                   frontend::cursor_parser<boost::spirit::istream_iterator>(),
+                   boost::spirit::qi::blank, v)) {
+    for (auto &c : v) {
       std::cout << boost::fusion::as_vector(c) << std::endl;
-      std::cerr << c.file << std::endl;
-    } else {
-      std::cerr << "failed!" << std::endl;
-      return 1;
     }
+    // if you try cat file2 | ./spirit_csv, it won't parse all the way to the
+    // end of the input, so this inequality will be true. if you do
+    // cat file | ./spirit_csv, the cursor will be right before the end of the
+    // input (it won't parse the final newline), so ++start == end
+    if (++start != end) {
+      std::cerr << "lol" << std::endl;
+    }
+    std::cerr << "success!" << std::endl;
+    return 0;
+  } else {
+    if (++start == end) {
+      std::cerr << "asdf" << std::endl;
+    }
+    std::cout << v.size() << std::endl;
+    std::cerr << "failure!" << std::endl;
+    return 1;
   }
-  std::cerr << "completed!" << std::endl;
-  return 0;
 }
