130138c4bb1f283865f3a4d037bb31a976745c74
Danny McClanahan <danieldmcclanahan@gmail.com>
2016-12-20 17:58:28 -0500

    harder better faster stronger

--- bash/utils.sh
+++ bash/utils.sh
@@ -3,7 +3,14 @@
 set -o pipefail
 set -o errexit
 
-handle_error_impl() {
+function print_args {
+  argc="$#"
+  argv="$@"
+  paste -d '' <(seq -f '$%g=' 1 "$argc") <(printf "'%s'\n" $argv)
+}
+export -f print_args
+
+function handle_error_impl {
   res="$1"
   if [ "$res" -ne 0 ]; then
     "${@:2}" "$res"
@@ -12,12 +19,12 @@ handle_error_impl() {
 }
 export -f handle_error_impl
 
-handle_error_fun() {
+function handle_error_fun {
   handle_error_impl "$?" "$@"
 }
 export -f handle_error_fun
 
-handle_print_string() {
+function handle_print_string {
   stderr="$1"
   stdout="$2"
   [ -z "$stderr" ] || echo "$stderr" >&2
@@ -25,7 +32,7 @@ handle_print_string() {
 }
 export -f handle_print_string
 
-handle_error_str() {
+function handle_error_str {
   handle_error_impl "$?" handle_print_string "$1" "$2"
 }
 export -f handle_error_str
