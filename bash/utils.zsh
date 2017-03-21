c48b64123593e5565387fe7a69972afd481e8fdc
Danny McClanahan <danieldmcclanahan@gmail.com>
2017-03-20 22:14:38 -0500

    add the script that will change the world

--- bash/utils.zsh
+++ bash/utils.zsh
@@ -1,14 +1,8 @@
-#!/bin/bash
-
-set -o pipefail
-set -o errexit
-
 function print_args {
   argc="$#"
   argv="$@"
   paste -d '' <(seq -f '$%g=' 1 "$argc") <(printf "'%s'\n" $argv)
 }
-export -f print_args
 
 function handle_error_impl {
   res="$1"
@@ -17,12 +11,10 @@ function handle_error_impl {
   fi
   return "$res"
 }
-export -f handle_error_impl
 
 function handle_error_fun {
   handle_error_impl "$?" "$@"
 }
-export -f handle_error_fun
 
 function handle_print_string {
   stderr="$1"
@@ -30,9 +22,23 @@ function handle_print_string {
   [ -z "$stderr" ] || echo "$stderr" >&2
   echo -n "$stdout"
 }
-export -f handle_print_string
 
 function handle_error_str {
   handle_error_impl "$?" handle_print_string "$1" "$2"
 }
-export -f handle_error_str
+
+function env_or_default {
+  varname="$1"
+  default="$2"
+  regex="$3"
+  if [[ -v "$varname" ]]; then
+    exp="${(P)varname}"
+    if [[ -n "$regex" && "$exp" =~ "$regex" ]]; then
+      echo "$exp"
+    else
+      echo "$default"
+    fi
+  else
+    echo "$default"
+  fi
+}
